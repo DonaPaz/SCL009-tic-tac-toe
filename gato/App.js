ff89c325412a2c50b6236a73565851784300d30e
donapaz <danielapaz.pl@gmail.com>
2019-09-05 16:59:18 -0400

    fix bug

--- gato/App.js
+++ gato/App.js
@@ -7,22 +7,21 @@ export default class App extends Component {
     <View style={styles.container}>
     
       <View style={{flexDirection:"row"}}>
-        <View style={[styles.title, { borderLeftWidth:0, borderTopWidth:0 }]}/>
-        <View style={[styles.title, { borderTopWidth:0 }]}/>
-        <View style={[styles.title, { borderTopWidth:0, borderRightWidth:0 }]}/>
+        <View style={[styles.grid, { borderLeftWidth:0, borderTopWidth:0 }]}/>
+        <View style={[styles.grid, { borderTopWidth:0 }]}/>
+        <View style={[styles.grid, { borderTopWidth:0, borderRightWidth:0 }]}/>
+      </View>
+     
+      <View style={{flexDirection:"row"}}>
+        <View style={[styles.grid, { borderLeftWidth:0 }]}/>
+        <View style={styles.grid}/>
+        <View style={[styles.grid, { borderRightWidth:0 }]}/>   
       </View>
 
       <View style={{flexDirection:"row"}}>
-        <View style={[styles.title, { borderLeftWidth:0 }]}/>
-        <View style={styles.title}/>
-        <View style={[styles.title, { borderRightWidth:0 }]}/>
-        
-      </View>
-
-      <View style={{flexDirection:"row"}}>
-        <View style={[styles.title, { borderLeftWidth:0, borderBottomWidth:0 }]}/>
-        <View style={[styles.title, { borderBottomWidth:0 }]}/>
-        <View style={[styles.title, { borderBottomWidth:0, borderRightWidth:0 }]}/>
+        <View style={[styles.grid, { borderLeftWidth:0, borderBottomWidth:0 }]}/>
+        <View style={[styles.grid, { borderBottomWidth:0 }]}/>
+        <View style={[styles.grid, { borderBottomWidth:0, borderRightWidth:0 }]}/>
       </View>
 
     </View>
@@ -38,10 +37,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   
-  title: {
+  grid: {
     borderWidth: 5,
     borderColor: '#7a0280',
     width:100,
     height:100,
   }
+
 });
